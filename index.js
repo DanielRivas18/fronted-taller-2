9d12b318d078a612595f7bcb68be67c22a8458a0
DanielRivas18 <jose.rivas@uptc.edu.co>
2024-06-10 21:18:59 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ const path = require('path');
 const app = express();
 const port = 3200;
 
-
+// Middleware para servir archivos estáticos desde la carpeta 'views'
 app.use(express.static(path.join(__dirname, 'views')));
 
-
+// Ruta para la página principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+// Ruta para la página 'listado.html'
+app.get('/listado.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'listado.html'));
+});
 
+// Escuchar en el puerto especificado
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 });
 
+// Middleware para servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
